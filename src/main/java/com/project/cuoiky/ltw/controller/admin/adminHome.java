0aeacd8d29aa34db113b3557d776749f48f2ea96
ATuan89 <18130272@st.hcmuaf.edu.vn>
2022-12-14 16:47:14 +0700

    made Admin home

---
+++ src/main/java/com/project/cuoiky/ltw/controller/admin/adminHome.java
@@ -0,0 +1,22 @@
+package com.project.cuoiky.ltw.controller.admin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "adminHome", value = "/admin")
+public class adminHome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getServletContext().
+                getRequestDispatcher("/Admin-page/pluto/index.jsp");
+        dispatcher.forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
